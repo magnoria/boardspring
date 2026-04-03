30af0a5696cd9739ebaffed2a3b906e552dfef1a
magnoria <sidaumv@gmail.com>
2026-04-04 00:18:05 +0900

    초기 프로젝트 폴더 구성 나중에 loombook 사용할것

--- src/main/java/com/sb/boardspring/domain/User.java
+++ src/main/java/com/sb/boardspring/domain/User.java
@@ -2,6 +2,9 @@ package com.sb.boardspring.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="users")
 public class User {
@@ -17,4 +20,47 @@ public class User {
 
     private String password;
 
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+
+    protected User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
